c4c65e40467371021b988da08ff86f86c6c7d5ac
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-24 23:06:28 +0300

    Add files via upload

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Berkan Aydın on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
